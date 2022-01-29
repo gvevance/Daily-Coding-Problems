3b55d36324771c52be2e5e1a111a1194fa7f084f
gvevance <gvevance@gmail.com>
2022-01-29 10:24:15 +0530

    library

--- DailyCodingProblem.py
+++ DailyCodingProblem.py
@@ -6,81 +6,9 @@
 # 3 Set up a system where mails are appropriately sorted base on difficulty, company. Implement a search functiionality (dictionary?)
 # 4 Push my solution to git with proper formatting (comments, file names, etc)
 
-import imaplib,email,pickle
+from methods import *
 from os.path import exists
-
-imap_url = "imap.gmail.com"
-
-folder_name = '"daily coding problem"'      # enclose the mailbox inside single quotes and double quotes
-pickle_file = 'DCP_object.pkl'
-credfile = 'user_pw.txt'
-
-
-def getUserCredentials():
-    error = False
-    try :
-        with open(credfile,'r') as cfile :
-            username = cfile.readline().strip()
-            password = cfile.readline().strip()
-            
-    except FileNotFoundError:
-        print(f"File {credfile} does not exist. Cannot login without username and password.")
-        print("Exiting ...")
-        error=True
-
-    except :
-        print("Unknown error. Aborting ...")
-        exit()
-
-    if error :
-        exit()
-        
-    return username,password
-
-
-def connect_to_server(imap_url,user,password):
-    ''' connecting to server '''
-    connection = imaplib.IMAP4_SSL(imap_url)
-    try :
-        connection.login(user,password)
-
-    except :
-        print(f"Could not connect to server with the given credentials. Check code or {credfile}. Exiting now ...")
-        exit()
-    
-    return connection
-
-
-def search_by_problem_num(imap_Obj,problem_num):
-    ''' search for the email which has {problem_num} in its subject '''
-
-    if problem_num > 600 :      
-        # TODO : a way to keep track of the last problem number received. Or change this number after a year :P
-        print("Error in problem number. Exiting ... ")
-        exit()
-
-    print(f"Searching for problem number : {problem_num} ...")
-    search_criterion = f'(SUBJECT "Daily Coding Problem: Problem #{str(problem_num)}")'
-    _, search_data = imap_Obj.search("utf-8", search_criterion)
-
-    byte_ = search_data[0]
-    _,data = imap_Obj.fetch(byte_,'(RFC822)')
-    _,b = data[0]
-    email_message = email.message_from_bytes(b)
-    difficulty_rating = email_message['subject'].split('[')[1][:-2]
-    
-    spl = "--------------------------------------------------------------------------------"
-    # Their mails have this separator
-
-    for part in email_message.walk():
-        if part.get_content_type()== "text/plain":
-            body = part.get_payload(decode = True)
-            text_list = body.decode().split(spl)[0].strip().split('\r\n\r\n')
-            company_name = text_list[1].split(" asked by ")[-1][:-1]
-            problem_statement = '\r\n\r\n'.join(text_list[2:])
-    
-    return problem_statement,company_name,difficulty_rating
-
+import pickle
 
 def update_pickle_file(problem_num,comp,company_dict,verbose = False):
     ''' update the file to reflect the most recent problem solved'''
--- reset_pickle.py
+++ reset_pickle.py
@@ -1,78 +1,8 @@
 ''' To reset pickle to the state we want (problem number) in case of any mismatch.
 Populate the company list too if required.
 '''
-
-import imaplib,email,pickle
-
-imap_url = "imap.gmail.com"
-
-folder_name = '"daily coding problem"'      # enclose the mailbox inside single quotes and double quotes
-pickle_file = 'DCP_object.pkl'
-credfile = 'user_pw.txt'
-
-
-def getUserCredentials():
-    ''' Get credentials from the user_pw.txt file'''
-    try :
-        with open(credfile,'r') as cfile :
-            username = cfile.readline().strip()
-            password = cfile.readline().strip()
-            
-    except FileNotFoundError:
-        print(f"File {credfile} does not exist. Cannot login without username and password.")
-        print("Exiting ...")
-        exit()
-
-    except :
-        print("Unknown error. Aborting ...")
-        exit()
-
-    return username,password
-
-
-def connect_to_server(imap_url,user,password):
-    ''' connecting to server '''
-    connection = imaplib.IMAP4_SSL(imap_url)
-    try :
-        connection.login(user,password)
-
-    except :
-        print(f"Could not connect to server with the given credentials. Check code or {credfile}. Exiting now ...")
-        exit()
-    
-    return connection
-
-
-def search_by_problem_num(imap_Obj,problem_num):
-    ''' search for the email which has "problem_num" in its subject '''
-
-    if problem_num > 600 :      
-        # TODO : a way to keep track of the last problem number received. Or change this number after a year :P
-        print("Error in problem number. Exiting ... ")
-        exit()
-
-    print(f"Searching for problem number : {problem_num} ...")
-    search_criterion = f'(SUBJECT "Daily Coding Problem: Problem #{str(problem_num)}")'
-    _, search_data = imap_Obj.search("utf-8", search_criterion)
-
-    byte_ = search_data[0]
-    _,data = imap_Obj.fetch(byte_,'(RFC822)')
-    _,b = data[0]
-    email_message = email.message_from_bytes(b)
-    difficulty_rating = email_message['subject'].split('[')[1][:-2]
-    
-    spl = "--------------------------------------------------------------------------------"
-    # Their mails have this separator
-
-    for part in email_message.walk():
-        if part.get_content_type()== "text/plain":
-            body = part.get_payload(decode = True)
-            text_list = body.decode().split(spl)[0].strip().split('\r\n\r\n')
-            company_name = text_list[1].split(" asked by ")[-1][:-1]
-            problem_statement = '\r\n\r\n'.join(text_list[2:])
-    
-    return problem_statement,company_name,difficulty_rating
-
+from methods import *
+import pickle
 
 def populate_companies_getDict(problem_num):
     
